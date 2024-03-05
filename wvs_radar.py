ced55326235676da79623c0d387cb8af7f1393e1
fraspecial <48252273+fraspecial@users.noreply.github.com>
2024-03-05 16:22:06 +0100

    Update wvs_radar.py
    Edited font

--- wvs_radar.py
+++ wvs_radar.py
@@ -126,6 +126,7 @@ def update_chart(countries, areas):
     # Set layout parameters
     fig.update_layout(
         autosize=False,
+        font_family='Roboto',
         margin=dict(l=10,r=10,t=10,b=10),
         polar=dict(radialaxis=dict(visible=True, range=[0,3])),
         showlegend=True,
